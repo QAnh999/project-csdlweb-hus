5bbfdf496de72dc6165c8e957619c38ee03d21cf
ddquynh <23001555@hus.edu.vn>
2025-10-13 20:01:55 +0700

    first update Oct 13

--- main.js
+++ main.js
@@ -1,21 +1,21 @@
 // menu button
-const menuBtn = document.getElementById("menu-btn");
-const navLinks = document.getElementById("nav-links");
+// const menuBtn = document.getElementById("menu-btn");
+// const navLinks = document.getElementById("nav-links");
 
-if (menuBtn && navLinks) {
-  const menuBtnIcon = menuBtn.querySelector("i");
+// if (menuBtn && navLinks) {
+//   const menuBtnIcon = menuBtn.querySelector("i");
 
-  menuBtn.addEventListener("click", () => {
-    navLinks.classList.toggle("open");
-    const isOpen = navLinks.classList.contains("open");
-    menuBtnIcon.setAttribute("class", isOpen ? "ri-close-line" : "ri-menu-line");
-  });
+//   menuBtn.addEventListener("click", () => {
+//     navLinks.classList.toggle("open");
+//     const isOpen = navLinks.classList.contains("open");
+//     menuBtnIcon.setAttribute("class", isOpen ? "ri-close-line" : "ri-menu-line");
+//   });
 
-  navLinks.addEventListener("click", () => {
-    navLinks.classList.remove("open");
-    menuBtnIcon.setAttribute("class", "ri-menu-line");
-  });
-}
+//   navLinks.addEventListener("click", () => {
+//     navLinks.classList.remove("open");
+//     menuBtnIcon.setAttribute("class", "ri-menu-line");
+//   });
+// }
 
 // an hien ngay ve 
 function updateReturnDate() {
