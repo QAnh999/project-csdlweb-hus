b2cb9aac31fdc8d423a8f739dc04aaac381916fa
ddquynh <23001555@hus.edu.vn>
2025-10-27 19:03:40 +0700

    second update Oct 27

--- javascript/payment.js
+++ javascript/payment.js
@@ -39,19 +39,17 @@ function handlePaymentPage() {
     flightDetailsEl.innerHTML = `
     <div class="flight">
         <h3>Chi tiết chuyến bay</h3>
-        <div class="detail-row"><span class="label">Mã chuyến bay:</span> <span>${flight.code} (${flight.type})</span></div>
-        <div class="detail-row"><span class="label">Chặng bay:</span> <span>${flight.airport_from} ✈ ${flight.airport_to}</span></div>
-        <div class="detail-row"><span class="label">Khởi hành:</span> <span>${flight.time_from}</span></div>
-        <div class="detail-row"><span class="label">Ghế đã chọn:</span> <span>${seat}</span></div>
-        
-        <hr style="margin: 15px 0;">
-        
+        <div class="detail-row"><span class="label">Mã chuyến bay:</span> <span class="info">${flight.code} (${flight.type})</span></div>
+        <div class="detail-row"><span class="label">Chặng bay:</span> <span class="info">${flight.airport_from} ✈ ${flight.airport_to}</span></div>
+        <div class="detail-row"><span class="label">Khởi hành:</span> <span class="info">${flight.time_from}</span></div>
+        <div class="detail-row"><span class="label">Ghế đã chọn:</span> <span class="info">${seat}</span></div>
+    
         <h3>Thông tin hành khách</h3>
-        <div class="detail-row"><span class="label">Họ tên:</span> <span> (${passenger.Danh_xung}) ${passenger.Ho} ${passenger.Ten_dem_va_ten}</span></div>
-        <div class="detail-row"><span class="label">Email:</span> <span>${passenger.Email}</span></div>
-        <div class="detail-row"><span class="label">Điện thoại:</span> <span>${passenger.Ma_quoc_gia} ${passenger.So_dien_thoai.replace(/^0/, '')}</span></div>
-        <div class="detail-row"><span class="label">Dịch vụ (Hành lý):</span> <span>${services.baggage.type}</span></div>
-        <div class="detail-row"><span class="label">Dịch vụ (Bữa ăn):</span> <span>${services.meal.type}</span></div>
+        <div class="detail-row"><span class="label">Họ tên:</span> <span class="info"> (${passenger.Danh_xung}) ${passenger.Ho} ${passenger.Ten_dem_va_ten}</span></div>
+        <div class="detail-row"><span class="label">Email:</span> <span class="info">${passenger.Email}</span></div>
+        <div class="detail-row"><span class="label">Điện thoại:</span> <span class="info">${passenger.Ma_quoc_gia} ${passenger.So_dien_thoai.replace(/^0/, '')}</span></div>
+        <div class="detail-row"><span class="label">Dịch vụ (Hành lý):</span> <span class="info">${services.baggage.type}</span></div>
+        <div class="detail-row"><span class="label">Dịch vụ (Bữa ăn):</span> <span class="info">${services.meal.type}</span></div>
     </div>
     `;
 
@@ -60,16 +58,15 @@ function handlePaymentPage() {
     // -----------------------------------------------------------------
     costSummaryEl.innerHTML = `
     <div class="services">
-        <div class="detail-row"><span class="label">Giá vé cơ bản:</span> <span>${basePrice.toLocaleString('vi-VN')} VNĐ</span></div>
-        <div class="detail-row"><span class="label">Phí hành lý:</span> <span>${baggagePrice.toLocaleString('vi-VN')} VNĐ</span></div>
-        <div class="detail-row"><span class="label">Phí bữa ăn:</span> <span>${mealPrice.toLocaleString('vi-VN')} VNĐ</span></div>
-        <div class="detail-row"><span class="label">Phụ thu dịch vụ hệ thống:</span> <span>${system_service.toLocaleString('vi-VN')} VNĐ</span></div>
-        <div class="detail-row"><span class="label">Phụ thu quản trị hệ thống:</span> <span>${service_management.toLocaleString('vi-VN')} VNĐ</span></div>
-        <div class="detail-row"><span class="label">Phí sân bay quốc nội:</span> <span>${airport_fee.toLocaleString('vi-VN')} VNĐ</span></div>
-        <div class="detail-row"><span class="label">Phí an ninh soi chiếu:</span> <span>${security_screening.toLocaleString('vi-VN')} VNĐ</span></div>
-        <div class="detail-row"><span class="label">Thuế giá trị gia tăng:</span> <span>${value_added_tax.toLocaleString('vi-VN')} VNĐ</span></div>
-        <hr>
-        <div class="detail-row" style="font-size: 1.2em; font-weight: bold;"><span class="label">Tổng:</span> <span>${totalAmount.toLocaleString('vi-VN')} VNĐ</span></div>
+        <div class="detail-row"><span class="label">Giá vé cơ bản:</span> <span class="price">${basePrice.toLocaleString('vi-VN')} VNĐ</span></div>
+        <div class="detail-row"><span class="label">Phí hành lý:</span> <span class="price">${baggagePrice.toLocaleString('vi-VN')} VNĐ</span></div>
+        <div class="detail-row"><span class="label">Phí bữa ăn:</span> <span class="price">${mealPrice.toLocaleString('vi-VN')} VNĐ</span></div>
+        <div class="detail-row"><span class="label">Phụ thu dịch vụ hệ thống:</span> <span class="price">${system_service.toLocaleString('vi-VN')} VNĐ</span></div>
+        <div class="detail-row"><span class="label">Phụ thu quản trị hệ thống:</span> <span class="price">${service_management.toLocaleString('vi-VN')} VNĐ</span></div>
+        <div class="detail-row"><span class="label">Phí sân bay quốc nội:</span> <span class="price">${airport_fee.toLocaleString('vi-VN')} VNĐ</span></div>
+        <div class="detail-row"><span class="label">Phí an ninh soi chiếu:</span> <span class="price">${security_screening.toLocaleString('vi-VN')} VNĐ</span></div>
+        <div class="detail-row"><span class="label">Thuế giá trị gia tăng:</span> <span class="price">${value_added_tax.toLocaleString('vi-VN')} VNĐ</span></div>
+        <div class="detail-row"><span class="label">Tổng:</span> <span class="price">${totalAmount.toLocaleString('vi-VN')} VNĐ</span></div>
     </div>
         `;
 
